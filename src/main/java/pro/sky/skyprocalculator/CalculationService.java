2c3073d4aaa697974459bd25bc8a3e180b7b6ab2
Денис <garaevdenis2019@gmail.com>
2023-05-08 15:01:07 +0500

    Сделано ДЗ

--- src/main/java/pro/sky/skyprocalculator/CalculationService.java
+++ src/main/java/pro/sky/skyprocalculator/CalculationService.java
@@ -2,10 +2,7 @@ package pro.sky.skyprocalculator;
 
 public interface CalculationService {
     Integer plus(Integer num1, Integer num2);
-
     Integer minus(Integer num1, Integer num2);
-
     Integer multiply(Integer num1, Integer num2);
-
     Integer divide(Integer num1, Integer num2);
 }
