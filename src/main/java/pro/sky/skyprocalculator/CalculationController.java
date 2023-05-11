529eb39240dcd6216647cf7d27e1be7d12b0ee1b
Денис <garaevdenis2019@gmail.com>
2023-05-11 20:12:29 +0500

    Сделано ДЗ

--- src/main/java/pro/sky/skyprocalculator/CalculationController.java
+++ src/main/java/pro/sky/skyprocalculator/CalculationController.java
@@ -23,50 +23,34 @@ public class CalculationController {
         return "Welcome to calculator";
     }
 
-    @GetMapping("/plusNumber")
-    public String plusNumber(@RequestParam String num1, @RequestParam String num2) {
-        return calculationService.plusNumber(num1, num2);
-    }
-
     @GetMapping("/plus")
-    public Integer plus(@RequestParam Integer num1, @RequestParam Integer num2) {
-        return calculationService.plus(num1, num2);
+    public String plus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        int result = calculationService.plus(num1, num2);
+        return num1 + "+" + num2 + "=" + result;
 
-    }
-
-    @GetMapping("/minusNumber")
-    public String minusNumber(@RequestParam String num1, @RequestParam String num2) {
-        return calculationService.minusNumber(num1, num2);
     }
 
     @GetMapping("/minus")
-    public Integer minus(@RequestParam Integer num1, @RequestParam Integer num2) {
-        return calculationService.minus(num1, num2);
+    public String minus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        int result = calculationService.minus(num1, num2);
+        return num1 + "-" + num2 + "=" + result;
 
-    }
-
-    @GetMapping("/multiplyNumber")
-    public String multiplyNumber(@RequestParam String num1, @RequestParam String num2) {
-        return calculationService.multiplyNumber(num1, num2);
     }
 
     @GetMapping("/multiply")
-    public Integer multiply(@RequestParam Integer num1, @RequestParam Integer num2) {
-        return calculationService.multiply(num1, num2);
+    public String multiply(@RequestParam Integer num1, @RequestParam Integer num2) {
+        int result = calculationService.multiply(num1, num2);
+        return num1 + "*" + num2 + "=" + result;
 
-    }
-
-    @GetMapping("/divideNumber")
-    public String divideNumber(@RequestParam String num1, @RequestParam String num2) {
-        return calculationService.divideNumber(num1, num2);
     }
 
     @GetMapping("/divide")
-    public Integer divide(@RequestParam Integer num1, @RequestParam Integer num2) {
+    public String divide(@RequestParam Integer num1, @RequestParam Integer num2) {
         if (num1 == 0 || num2 == 0) {
             throw new IllegalArgumentException("We can not divide zero");
         }
-        return calculationService.divide(num1, num2);
+        int result = calculationService.divide(num1, num2);
+        return num1 + "/" + num2 + "=" + result;
     }
 }
 
--- src/main/java/pro/sky/skyprocalculator/impl/CalculationServiceImpl.java
+++ src/main/java/pro/sky/skyprocalculator/impl/CalculationServiceImpl.java
@@ -7,18 +7,10 @@ import pro.sky.skyprocalculator.CalculationService;
 
 @Service
 public class CalculationServiceImpl implements CalculationService {
-    @Override
-    public String plusNumber(String num1, String num2) {
-        return num1 + "+" + num2 + "=10";
-    }
+
     @Override
     public Integer plus(Integer num1, Integer num2) {
         return num1 + num2;
-    }
-
-    @Override
-    public String minusNumber(String num1, String num2) {
-        return num1 + "-" + num2 + "=0";
     }
 
     @Override
@@ -27,19 +19,10 @@ public class CalculationServiceImpl implements CalculationService {
     }
 
     @Override
-    public String multiplyNumber(String num1, String num2) {
-        return num1 + "*" + num2 + "=25";
-    }
-
-    @Override
     public Integer multiply(Integer num1, Integer num2) {
         return num1 * num2;
     }
 
-    @Override
-    public String divideNumber(String num1, String num2) {
-        return num1 + "/" + num2 + "=1";
-    }
 
     @Override
     public Integer divide(Integer num1, Integer num2) {
